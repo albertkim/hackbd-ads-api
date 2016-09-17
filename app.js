ce164dc3cbbbdec41b65b4779c5461e8e2af72cb
albertkim <albert275@gmail.com>
2016-09-17 16:38:05 -0700

    Working randomized ads distribution

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const expressHandlebars  = require('express-handlebars')
-const initalState = require('./initalState')
 
+const initalState = require('./initalState')
 const port = 444
 const app = express()
 
+console.log(initalState)
+
+let imageUrlArray = []
+Object.keys(initalState).forEach(function(skuId) {
+  imageUrlArray.push(initalState[skuId].imageUrl)
+})
+console.log(imageUrlArray)
+
 app.engine('handlebars', expressHandlebars())
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use('/public', express.static('public'))
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, access_token, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  next()
+})
 
 app.get('/product/:productId/images', function(req, res, next) {
 
@@ -17,11 +34,11 @@ app.post('/product/:productId/image', function(req, res, next) {
 })
 
 app.get('/ad', function(req, res, next) {
-  const url = 'http://www.vancitybuzz.com/wp-content/uploads/2014/01/Screen-Shot-2014-01-21-at-12.36.41-PM.png'
+  var randomImageUrl = imageUrlArray[Math.floor(Math.random() * imageUrlArray.length)];
   const referralId = 'asdf'
 
   res.render('ad', {
-    url: url,
+    url: randomImageUrl,
     referralId: referralId
   })
 })
